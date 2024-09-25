e80004dadec2261d63874e864fd6e116ed56a4e0
허강현 <hgh6128@gmail.com>
2024-09-25 13:46:32 +0900

    generateStaticParams 구현 연습

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -1,16 +1,16 @@
-'use client';
-
 type Props = {
     params: {
         category: string;
-    }
-}
-export default function page({params} : Props) {
+    };
+};
+export default function page({ params }: Props) {
     console.log(params);
-    return (
-        <div>
-            {params.category}
-        </div>
-    );
+    return <div>{params.category}</div>;
 }
 
+export function generateStaticParams() {
+    const categories = ["javascript", "react"];
+    return categories.map((category) => ({
+        category: category,
+    }));
+}
